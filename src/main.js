e9628fe38f96a4438a9472d4db503a78d497eecb
Daniel Buchner <danieljb2@gmail.com>
2016-04-13 09:39:20 -0700

    account for delay

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
   var replaceSpaces = / /g,
       captureTimes = /(\d|\d+?[.]?\d+?)(s|ms)(?!\w)/gi,
       transPre = 'transition' in getComputedStyle(document.documentElement) ? 't' : xtag.prefix.js + 'T',
+      transDel = transPre + 'ransitionDelay',
       transDur = transPre + 'ransitionDuration',
-      transProp = transPre + 'ransitionProperty',
       ready = document.readyState == 'complete' ?
         xtag.skipFrame(function(){ ready = false }) :
         xtag.addEvent(document, 'readystatechange', function(){
@@ -12,6 +12,15 @@
             xtag.removeEvent(document, 'readystatechange', ready);
           }
         });
+
+  function parseTimes(style){
+    var value = 0;
+    style.replace(captureTimes, function(match, time, unit){
+      time = parseFloat(time) * (unit === 's' ? 1000 : 1);
+      if (time >= value) value = time;
+    });
+    return value;
+  }
 
   function startTransition(node, name, transitions){
     var current = node.getAttribute('transition');
@@ -23,11 +32,8 @@
         max = transition.max;
 
     if (isNaN(max)) {
-      max = transition.max = 0;
-      getComputedStyle(node)[transDur].replace(captureTimes, function(match, time, unit){
-        time = parseFloat(time) * (unit === 's' ? 1000 : 1);
-        if (time >= max) max = transition.max = time;
-      });
+      var styles = getComputedStyle(node);
+      max = transition.max = parseTimes(styles[transDel]) + parseTimes(styles[transDur]);
     }
 
     transition.timer = setTimeout(function(){
